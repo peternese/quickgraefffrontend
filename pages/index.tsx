976cedec7243b78c053ec118be87002c53521fd5
peternese <petergraeff215@googlemail.com>
2025-03-11 20:59:50 +0100

    powered by peternese

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,6 +97,7 @@ export default function Home() {
             <div className={`fixed inset-0 bg-black bg-opacity-80 flex flex-col justify-center items-center text-white ${loading ? "fade-in" : "fade-out"}`}>
               <span className="loader"></span>
               <p className="mt-4 text-lg font-semibold">Bitte warten Sie...</p>
+              <p className="powered">brought to you by peternese</p>
             </div>
           )}
         </div>
